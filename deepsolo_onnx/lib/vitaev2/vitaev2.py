41ae726a259d059ff7a7a48ec5afb839e19f9045
Alexander Goryunov <alex.goryunov@gmail.com>
2023-11-01 18:04:09 +0200

    .

--- deepsolo_onnx/lib/vitaev2/vitaev2.py
+++ deepsolo_onnx/lib/vitaev2/vitaev2.py
@@ -1,15 +1,99 @@
-from abc import ABCMeta, abstractmethod
 from functools import partial
 
 import numpy as np
 
 import torch
 from torch import nn
+import torch.utils.checkpoint as checkpoint
 
 from timm.models.layers import trunc_normal_
 
+from .NormalCell import NormalCell
+from .ReductionCell import ReductionCell
 from .utils import ShapeSpec
-from .swin_transformer import BasicLayer, Backbone
+from .swin_transformer import Backbone
+
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, inter_channel=32, out_channels=48, img_size=None):
+        self.img_size = img_size
+        self.inter_channel = inter_channel
+        self.out_channel = out_channels
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, inter_channel, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(inter_channel),
+            nn.ReLU(inplace=True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(inter_channel, out_channels, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x, size):
+        x = self.conv3(self.conv2(self.conv1(x)))
+        b, c, h, w = x.shape
+        x = x.permute(0, 2, 3, 1).reshape(b, h * w, c)
+        return x, (h, w)
+
+    def flops(self, ) -> float:
+        flops = 0
+        flops += 3 * self.inter_channel * self.img_size[0] * self.img_size[1] // 4 * 9
+        flops += self.img_size[0] * self.img_size[1] // 4 * self.inter_channel
+        flops += self.inter_channel * self.out_channel * self.img_size[0] * self.img_size[1] // 16 * 9
+        flops += self.img_size[0] * self.img_size[1] // 16 * self.out_channel
+        flops += self.out_channel * self.out_channel * self.img_size[0] * self.img_size[1] // 16
+        return flops
+
+
+class BasicLayer(nn.Module):
+    def __init__(self, img_size=224, in_chans=3, embed_dims=64, token_dims=64, downsample_ratios=4, kernel_size=7, RC_heads=1, NC_heads=6, dilations=[1, 2, 3, 4],
+                RC_op='cat', RC_tokens_type='performer', NC_tokens_type='transformer', RC_group=1, NC_group=64, NC_depth=2, dpr=0.1, mlp_ratio=4., qkv_bias=True,
+                qk_scale=None, drop=0, attn_drop=0., norm_layer=nn.LayerNorm, class_token=False, gamma=False, init_values=1e-4, SE=False, window_size=7,
+                use_checkpoint=False):
+        super().__init__()
+        self.img_size = img_size
+        self.in_chans = in_chans
+        self.embed_dims = embed_dims
+        self.token_dims = token_dims
+        self.downsample_ratios = downsample_ratios
+        self.out_size = self.img_size // self.downsample_ratios
+        self.RC_kernel_size = kernel_size
+        self.RC_heads = RC_heads
+        self.NC_heads = NC_heads
+        self.dilations = dilations
+        self.RC_op = RC_op
+        self.RC_tokens_type = RC_tokens_type
+        self.RC_group = RC_group
+        self.NC_group = NC_group
+        self.NC_depth = NC_depth
+        self.use_checkpoint = use_checkpoint
+        if RC_tokens_type == 'stem':
+            self.RC = PatchEmbedding(inter_channel=token_dims//2, out_channels=token_dims, img_size=img_size)
+        elif downsample_ratios > 1:
+            self.RC = ReductionCell(img_size, in_chans, embed_dims, token_dims, downsample_ratios, kernel_size,
+                            RC_heads, dilations, op=RC_op, tokens_type=RC_tokens_type, group=RC_group, gamma=gamma, init_values=init_values, SE=SE)
+        else:
+            self.RC = nn.Identity()
+        self.NC = nn.ModuleList([
+            NormalCell(token_dims, NC_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale, drop=drop, attn_drop=attn_drop,
+                       drop_path=dpr[i] if isinstance(dpr, list) else dpr, norm_layer=norm_layer, class_token=class_token, group=NC_group, tokens_type=NC_tokens_type,
+                       gamma=gamma, init_values=init_values, SE=SE, img_size=img_size // downsample_ratios, window_size=window_size, shift_size=0)
+        for i in range(NC_depth)])
+
+    def forward(self, x, size):
+        h, w = size
+        x, (h, w) = self.RC(x, (h, w))
+        for nc in self.NC:
+            nc.H = h
+            nc.W = w
+            if self.use_checkpoint:
+                x = checkpoint.checkpoint(nc, x)
+            else:
+                x = nc(x)
+        return x, (h, w)
 
 
 class ViTAEv2(Backbone):
@@ -84,8 +168,9 @@ class ViTAEv2(Backbone):
                     self.RC_op[i], self.RC_tokens_type[i], self.NC_tokens_type[i], 
                     self.RC_group[i], self.NC_group[i], self.NC_depth[i], 
                     dpr[startDpr:self.NC_depth[i]+startDpr],
-                mlp_ratio=self.mlp_ratio[i], qkv_bias=self.qkv_bias[i], qk_scale=self.qk_scale[i], drop=self.drop[i], attn_drop=self.attn_drop[i],
-                norm_layer=self.norm_layer[i], window_size=window_size, use_checkpoint=use_checkpoint)
+                    mlp_ratio=self.mlp_ratio[i], qkv_bias=self.qkv_bias[i], qk_scale=self.qk_scale[i], drop=self.drop[i], attn_drop=self.attn_drop[i],
+                    norm_layer=self.norm_layer[i], window_size=window_size, use_checkpoint=use_checkpoint
+                )
             )
             img_size = img_size // self.downsample_ratios[i]
             in_chans = self.tokens_dims[i]

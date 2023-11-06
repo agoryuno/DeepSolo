5dee9e3664124bb7983a6999a410cec8c2e6b2ea
Alexander Goryunov <alex.goryunov@gmail.com>
2023-11-06 14:06:12 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,94 +1,36 @@
 #!/usr/bin/env python
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
-import glob
-import os
-from setuptools import find_packages, setup
+import sys
+from setuptools import setup
 import torch
-from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
 
 torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
-assert torch_ver >= [1, 3], "Requires PyTorch >= 1.3"
+assert torch_ver >= [2, 0], "Requires PyTorch >= 2.0"
 
+WHEEL_URLS = {(3,10): "https://github.com/agoryuno/adet_layers/raw/main/wheels/adet_layers-0.0.1-cp310-linux_x86_64.whl",
+              (3,11): "https://github.com/agoryuno/adet_layers/raw/main/wheels/adet_layers-0.0.1-cp311-linux_x86_64.whl"}
 
-def get_extensions():
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    extensions_dir = os.path.join(this_dir, "adet", "layers", "csrc")
+def get_wheel(wheels=WHEEL_URLS):
+    major = sys.version_info.major
+    minor = sys.version_info.minor
 
-    main_source = os.path.join(extensions_dir, "vision.cpp")
-    sources = glob.glob(os.path.join(extensions_dir, "**", "*.cpp"))
-    source_cuda = glob.glob(os.path.join(extensions_dir, "**", "*.cu")) + glob.glob(
-        os.path.join(extensions_dir, "*.cu")
-    )
-
-    sources = [main_source] + sources
-
-    extension = CppExtension
-
-    extra_compile_args = {"cxx": []}
-    define_macros = []
-
-    if (torch.cuda.is_available() and CUDA_HOME is not None) or os.getenv("FORCE_CUDA", "0") == "1":
-        extension = CUDAExtension
-        sources += source_cuda
-        define_macros += [("WITH_CUDA", None)]
-        extra_compile_args["nvcc"] = [
-            "-DCUDA_HAS_FP16=1",
-            "-D__CUDA_NO_HALF_OPERATORS__",
-            "-D__CUDA_NO_HALF_CONVERSIONS__",
-            "-D__CUDA_NO_HALF2_OPERATORS__",
-        ]
-
-        if torch_ver < [1, 7]:
-            # supported by https://github.com/pytorch/pytorch/pull/43931
-            CC = os.environ.get("CC", None)
-            if CC is not None:
-                extra_compile_args["nvcc"].append("-ccbin={}".format(CC))
-
-    sources = [os.path.join(extensions_dir, s) for s in sources]
-
-    include_dirs = [extensions_dir]
-
-    ext_modules = [
-        extension(
-            "adet._C",
-            sources,
-            include_dirs=include_dirs,
-            define_macros=define_macros,
-            extra_compile_args=extra_compile_args,
-        )
-    ]
-
-    return ext_modules
+    wheel_url = wheels.get((major, minor))
+    assert wheel_url, f"Couldn't locate a .whl file of the 'adet_layers' library for Python version {major}.{minor}"
+    return wheel_url
 
 
 setup(
-    name="AdelaiDet",
-    version="0.2.0",
-    author="Adelaide Intelligent Machines",
-    url="https://github.com/stanstarks/AdelaiDet",
-    description="AdelaiDet is AIM's research "
-    "platform for instance-level detection tasks based on Detectron2.",
-    packages=find_packages(exclude=("configs", "tests")),
-    python_requires=">=3.6",
+    name="deepsolo_onnx",
+    version="0.0.1",
+    author="Alex Goryunov",
+    url="https://github.com/agoryuno/deepsolo_onnx",
+    description="A stripped down version of the original "
+        " DeepSolo model's codebase, with most dependencies removed or "
+        " repackaged separately",
+    packages=["deepsolo_onnx"],
+    python_requires=">=3.10",
     install_requires=[
-        "termcolor>=1.1",
-        "Pillow>=6.0",
-        "yacs>=0.1.6",
-        "tabulate",
-        "cloudpickle",
-        "matplotlib",
-        "tqdm>4.29.0",
-        "tensorboard",
-        "rapidfuzz",
-        "Polygon3",
-        "shapely",
-        "scikit-image",
-        "editdistance",
-        "opencv-python",
-        "numba",
+        f"adet_layers @ {get_wheel()}"
     ],
-    extras_require={"all": ["psutil"]},
-    ext_modules=get_extensions(),
-    cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
+    extras_require={"all": ["psutil"]}
 )
